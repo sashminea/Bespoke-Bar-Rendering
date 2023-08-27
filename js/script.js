e4b3f69be2df13f64b4a8d388b28c3da1e3f4d77
Sashmin <sashminadhikari@gmail.com>
2023-08-27 19:56:00 +0545

    Basic Design Layout

---
+++ js/script.js
@@ -0,0 +1,16 @@
+document.addEventListener('DOMContentLoaded', function () {
+
+const navbar = document.querySelector('nav');
+
+function Sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+async function sleep(ms) {
+  await Sleep(ms);
+}
+
+
+
+
+});
