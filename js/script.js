6d44fe119027181f2dcec01bee9675bedb81e048
Sashmin <sashminadhikari@gmail.com>
2023-08-28 10:34:52 +0545

    Bar Creation on Click + Counter Added

--- js/script.js
+++ js/script.js
@@ -1,13 +1,26 @@
 document.addEventListener('DOMContentLoaded', function () {
+  const barButton = document.getElementById('bar');
+  const workspace = document.querySelector('.workspace');
+  var nBarsCount = 0; 
+  var nBars = document.getElementById('nBars');
 
-const navbar = document.querySelector('nav');
+  function Sleep(ms) {
+      return new Promise(resolve => setTimeout(resolve, ms));
+  }
 
-function Sleep(ms) {
-  return new Promise(resolve => setTimeout(resolve, ms));
-}
+  async function sleep(ms) {
+      await Sleep(ms);
+  }
 
-async function sleep(ms) {
-  await Sleep(ms);
-}
+  barButton.addEventListener('click', barCreation);
 
+  function barCreation() {
+
+      const newBar = document.createElement('div');
+      newBar.className = "newBar";
+      nBarsCount = nBarsCount + 1;
+
+      workspace.appendChild(newBar);
+      nBars.textContent = `${nBarsCount}`;
+  }
 });
